8ecc4d68a490261316fa007b0d56943018b4c057
cm val <cmvaltime@gmail.com>
2022-04-29 09:05:27 +0300

    feat: bst some methods add

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -8,29 +8,72 @@ const { NotImplementedError } = require('../extensions/index.js');
 */
 class BinarySearchTree {
 
+  constructor() {
+    this.treeRoot = null;
+  }
+
   root() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    return this.treeRoot;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
+  add(data) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    this.treeRoot = addData(this.treeRoot, data);
+
+    function addData(treeNode, data) {
+      return (!treenode) ? new Node(data) :
+             (treeNode.data === data) ? treeNode :
+             (data < treeNode.data) ? treeNode.left = addData(treeNode.left, data) :
+             treeNode.right = addData(treeNode.right, data)
+    }
+    return treeNode
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
+  has(data) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    
+    function searchNode(treeNode, data) {
+      return (!treeNode) ? false :
+             (treeNode.data === data) ? true :
+             (data < treeNode.data) ? searchNode(treeNode.left, data) :
+             searchNode(treeNode.right, data)
+    }
+  
+    return searchNode(this.treeRoot, data)
+  
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
+  find(data) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    function findNode(treeNode, data) {
+      return (!treeNode) ? null :
+             (treeNode.data === data) ? data :
+             (data < treeNode.data) ? findNode(treeNode.left, data) :
+             findNode(treeNode.right, data)
+    }
+  
+    return findNode(this.treeRoot, data)
+  
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
+  remove(data) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    this.treeRoot = removeNode(treeNode, data);
+
+    function removeNode(treeNode, data) {
+      if (!treeNode) {return null}
+
+      
+    }
+ 
+ 
+ 
   }
 
   min() {
