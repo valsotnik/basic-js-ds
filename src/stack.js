f11b42475dcd3c342651c0ea148e256d1eaa65c4
cm val <cmvaltime@gmail.com>
2022-04-29 11:16:42 +0300

    feat: stack solution add

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
+
+  constructor() {
+    this.stack = [];
+
+  }
+  push(el) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    this.stack.push(el);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    return this.stack.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    return this.stack[this.stack.length - 1]
   }
 }
 
